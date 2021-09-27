2fc013755d8195c7981319b0e3675331984f6844
zyc <1533637649@qq.com>
2021-09-27 09:06:55 +0800

    hot-fix commit2

--- src/main/java/com/zyc/crud/test/HelloGit.java
+++ src/main/java/com/zyc/crud/test/HelloGit.java
@@ -1,6 +1,8 @@
 package com.zyc.crud.test;
 
 import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author zyc
@@ -8,7 +10,43 @@ import java.sql.SQLOutput;
  */
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git by hot-fix");
+        int num = 15;
+        ArrayList<Integer> result = new ArrayList<>();
+        int[] nums = new int[num + 1];
+        for (int i = 0; i <= num; i++) {
+            nums[i] = i;
+        }
+
+        for (int j = 0; j < nums.length; j++) {
+            int count = 0;
+            ArrayList<Integer> list = int2String(nums[j]);
+            for (int i:list) {
+                if (i == 1) {
+                    count++;
+                }
+            }
+            result.add(count);
+        }
+
+        System.out.println(result);
+
     }
+
+    public static ArrayList<Integer> int2String(int num) {
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int shang = num / 2;
+            int yu = num % 2;
+
+            list.add(yu);
+            num = shang;
+
+            if (shang == 0) {
+                break;
+            }
+        }
+        return list;
+    }
+
+
 }
